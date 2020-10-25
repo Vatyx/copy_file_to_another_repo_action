ade59108bd386bb0aacf2ad86d9e723769844df9
Sahil Dhanju <Vatyx@users.noreply.github.com>
2020-10-25 17:18:51 +0000

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ execSync(clone);
 //execSync(`mkdir -p .//${process.env.INPUT_DESTINATION_FOLDER}`)
 execSync(`cp -R ${process.env.INPUT_SOURCE_FILE} homebrew-idk`)
 
-execSync(`cd homebrew-idk && git add . && git commit --message "Update from https://github.com/${process.env.GITHUB_REPOSITORY}/commit/${process.env.GITHUB_SHA} && git push -u origin HEAD:${process.env.OUTPUT_BRANCH}`)
+execSync(`cd homebrew-idk && git add . && git commit --message "Update from https://github.com/${process.env.GITHUB_REPOSITORY}/commit/${process.env.GITHUB_SHA}" && git push -u origin master`)
 //execSync(`git commit --message "Update from https://github.com/${process.env.GITHUB_REPOSITORY}/commit/${process.env.GITHUB_SHA}"`)
 //execSync(`echo "Pushing git commit"`)
 //execSync(`git push -u origin HEAD:${process.env.OUTPUT_BRANCH}`)
