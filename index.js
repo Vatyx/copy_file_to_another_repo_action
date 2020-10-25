7e31ab45a843bc1a04935a6c3fc5e4f81dae857a
Sahil Dhanju <Vatyx@users.noreply.github.com>
2020-10-25 17:13:10 +0000

    Update index.js

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ console.log("this happened");
 
 let temp = execSync(`mktemp -d`).toString();
 
-let clone = `git clone --single-branch --branch ${process.env.INPUT_DESTINATION_BRANCH} "https://${process.env.API_TOKEN_GITHUB}@github.com/${process.env.INPUT_DESTINATION_REPO}.git" "${temp}"`
+let clone = `git clone --single-branch --branch ${process.env.INPUT_DESTINATION_BRANCH} "https://${process.env.API_TOKEN_GITHUB}@github.com/${process.env.INPUT_DESTINATION_REPO}.git"`
 execSync(clone);
 
-execSync(`mkdir -p ${process.env.CLONE_DIR}/${process.env.INPUT_DESTINATION_FOLDER}`)
-execSync(`cp -R ${process.env.INPUT_SOURCE_FILE} "${process.env.CLONE_DIR}/${process.env.INPUT_DESTINATION_FOLDER}"`)
+//execSync(`mkdir -p .//${process.env.INPUT_DESTINATION_FOLDER}`)
+execSync(`cp -R ${process.env.INPUT_SOURCE_FILE} ${process.env.INPUT_DESTINATION_REPO}"`)
 
 execSync(`git add .`)
 execSync(`git commit --message "Update from https://github.com/${process.env.GITHUB_REPOSITORY}/commit/${process.env.GITHUB_SHA}"`)
