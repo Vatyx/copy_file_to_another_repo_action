9b6d3e34ef43ee809a1a83655729b230d3b4824a
Sahil Dhanju <Vatyx@users.noreply.github.com>
2020-10-25 16:31:30 +0000

    Create index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+var args = process.argv.slice(2)
+var util  = require('util'),
+    spawn = require('child_process').spawn,
+    ls    = spawn('entrypoint.sh', args); // the second arg is the command 
+                                          // options
+
+ls.stdout.on('data', function (data) {    // register one or more handlers
+  console.log('stdout: ' + data);
+});
+
+ls.stderr.on('data', function (data) {
+  console.log('stderr: ' + data);
+});
+
+ls.on('exit', function (code) {
+  console.log('child process exited with code ' + code);
+});
